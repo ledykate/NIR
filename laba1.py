1e4ec5b6c9e014310eaef06bc17bf3922efe175b
ledykate <katyakola97@gmail.com>
2020-03-14 21:10:46 +0300

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -13,9 +13,9 @@ rcParams['font.family']='fantasy'
 rcParams['font.fantasy']='Times New Roman'
 
 N=10
-M=10000
+M=2000
 P=[]
-p=0
+p=(1/N)/20
 P.append(p)
 T=[]
 while p<=(1/N):
@@ -25,25 +25,18 @@ while p<=(1/N):
 
 for i in range(len(P)):
     #frem=[]
-    S = []
-    Q=[0 for i in range(M)]
+    Q=[]
     for j in range(M):
         slot=[0 for i in range(N)]  
         for k in range(N):
             slot[k]=random.choices([1, 0], weights=[P[i],1-P[i]])[0]
-        s=sum(slot)
-        S.append(s)
-        Q[0]=0
+        #s=sum(slot)
+        Q.append(sum(slot))
         if Q[j-1]>0:
-            Q[j]=Q[j-1]-1+s
+            Q[j]=Q[j-1]-1+sum(slot)
         else:
-            Q[j]=s
-    if P[i]==0:
-        T.append(0)
-    else:
-        T.append((sum(Q)/len(Q))/P[i])
-        #T.append(sum(S)/M)
-        #T.append((sum(S)/M)/P[i])
+            Q[j]=sum(slot)
+    T.append((sum(Q)/len(Q))/P[i])
 
 fig = plt.figure()
 ax1 = fig.add_axes([0,1.2,1,1])
